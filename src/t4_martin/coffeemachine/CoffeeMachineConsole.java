e3fffa727078dbe296ce9c31e3dd66b3d4ea7078
kmagdic <karlo.magdic@gmail.com>
2024-11-11 16:08:18 +0100

    Initial commit

---
+++ src/t4_martin/coffeemachine/CoffeeMachineConsole.java
@@ -0,0 +1,120 @@
+package _karlo_dragan.coffeemachine;
+
+
+
+import java.util.Scanner;
+
+public class CoffeeMachineConsole {
+
+    Scanner sc = new Scanner(System.in);
+
+
+    public static void main(String[] args)  {
+        CoffeeMachineConsole console = new CoffeeMachineConsole();
+        console.run();
+    }
+
+    void run() {
+        CoffeeMachine machine = new CoffeeMachine(400, 540, 120, 9, 550);
+        System.out.println("Welcome to Coffee Machine 1.0 version by Karlo");
+        boolean startedSuccessfully = machine.start();
+
+        if(!startedSuccessfully) {
+            System.out.println("Coffee machine started but without file. Using default values.");
+        }
+
+        String action = "";
+
+        while (!action.equals("exit")) {
+            System.out.println("Write action (buy, login, exit): ");
+            action = sc.next();
+            switch (action) {
+                case "buy":
+                    buyAction(machine);
+                    break;
+
+                case "login":
+                    System.out.println("Enter username: ");
+                    String username = sc.next();
+                    System.out.println("Enter password: ");
+                    String password = sc.next();
+
+                    if (machine.login(username, password)) {
+                        adminMenu(machine);
+                    } else {
+                        System.out.println("Wrong username or password\n");
+                    }
+                    break;
+
+                case "exit":
+                    machine.stop();
+                    System.out.println("Shutting down the machine. Bye!");
+                    break;
+
+                default:
+                    System.out.println("No such option");
+            }
+        }
+    }
+
+    private void buyAction(CoffeeMachine machine) {
+        System.out.println("Choice: ");
+        CoffeeType[] coffeeTypes = machine.getCoffeeTypes();
+        for (int i = 0; i < machine.getCoffeeTypes().length; i++) {
+            System.out.println((i + 1) + " - " + coffeeTypes[i].getName());
+        }
+        System.out.println("Enter your choice: ");
+
+        int typeOfCoffeeChoice = sc.nextInt();
+        if (typeOfCoffeeChoice <= coffeeTypes.length) {
+            String msg = machine.buyCoffee(coffeeTypes[typeOfCoffeeChoice - 1]);
+            System.out.println(msg);
+        } else {
+            System.out.println("Wrong enter\n");
+        }
+    }
+
+    private void adminMenu(CoffeeMachine machine) {
+        String ch = "";
+        while (!ch.equals("exit")) {
+            System.out.println(" ");
+            System.out.println("Write action (fill, remaining, take, exit):");
+            ch = sc.next();
+
+            switch (ch) {
+                case "fill":
+                    System.out.println("Write how many ml of water you want to add:");
+                    int water = sc.nextInt();
+                    System.out.println("Write how many ml of milk you want to add:");
+                    int milk = sc.nextInt();
+                    System.out.println("Write how many grams of coffee beans you want to add:");
+                    int coffeeBeans = sc.nextInt();
+                    System.out.println("\"Write how many disposable cups you want to add: ");
+                    int cup = sc.nextInt();
+                    machine.fill(water, milk, coffeeBeans, cup);
+                    break;
+
+                case "take":
+                    float amount = machine.takeMoney();
+                    System.out.println("I gave you $" + amount + "\n");
+                    break;
+
+                case "remaining":
+                    System.out.println("The coffee machine has:");
+                    System.out.println(machine.getWater() + " ml of water");
+                    System.out.println(machine.getMilk() + " ml of milk");
+                    System.out.println(machine.getCoffeeBeans() + " g of water");
+                    System.out.println(machine.getCups() + " cups");
+                    System.out.println("$" + machine.getMoney() + " of money");
+                    break;
+
+                case "exit":
+                    break;
+
+            }
+        }
+    }
+
+
+
+}
---
+++ src/t4_martin/coffeemachine/CoffeeType.java
@@ -0,0 +1,68 @@
+package _karlo_dragan.coffeemachine;
+
+public class CoffeeType {
+
+    private int milkNeeded;
+    private int waterNeeded;
+    private int coffeeBeansNeeded;
+    private int price;
+    private String name;
+
+
+    public CoffeeType(String name, int waterNeeded, int milkNeeded, int coffeeBeansNeeded, int price) {
+        this.milkNeeded = milkNeeded;
+        this.waterNeeded = waterNeeded;
+        this.coffeeBeansNeeded = coffeeBeansNeeded;
+        this.price = price;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getMilkNeeded() {
+        return milkNeeded;
+    }
+
+    public void setMilkNeeded(int milkNeeded) {
+        this.milkNeeded = milkNeeded;
+    }
+
+    public int getWaterNeeded() {
+        return waterNeeded;
+    }
+
+    public void setWaterNeeded(int waterNeeded) {
+        this.waterNeeded = waterNeeded;
+    }
+
+    public int getCoffeeBeansNeeded() {
+        return coffeeBeansNeeded;
+    }
+
+    public void setCoffeeBeansNeeded(int coffeeBeansNeeded) {
+        this.coffeeBeansNeeded = coffeeBeansNeeded;
+    }
+}
+
+
+
+
+
+
+
+
+
