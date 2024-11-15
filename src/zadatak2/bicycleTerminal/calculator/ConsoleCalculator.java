ff35a57bfbd3b1c39139f410aee3aa5c1afdbe9f
Bojan Pađen <page0itv@gmail.com>
2024-11-15 10:37:51 +0100

    added stage 1 calculator

---
+++ src/zadatak2/bicycleTerminal/calculator/ConsoleCalculator.java
@@ -0,0 +1,41 @@
+package zadatak2.bicycleTerminal.calculator;
+
+import java.util.Scanner;
+
+public class ConsoleCalculator {
+
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+
+        System.out.println("Operacije osnovnog kalkultora:");
+        System.out.println("+ - / *\n");
+
+        while (true) {
+            String choice;
+            System.out.println("1- Calculate");
+            System.out.println("2 - Exit");
+            System.out.print("Enter:");
+            choice = scanner.next();
+
+            if (choice.equals("2")) {
+                return;
+            }
+            else if (choice.equals("1")){
+                enterNumbersAndOperation(calculator);
+            }
+        }
+    }
+
+    public static void enterNumbersAndOperation (Calculator calculator){
+
+        System.out.print("A: ");
+        calculator.setA(scanner.nextInt());
+        System.out.print("Op: ");
+        calculator.setOperation(scanner.next());
+        System.out.print("B: ");
+        calculator.setB(scanner.nextInt());
+        System.out.println(calculator.calculate());
+    }
+}
