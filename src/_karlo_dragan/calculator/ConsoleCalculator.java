050342b336aee065fc08485e24d87032fef1abac
drsacer <drsacer@gmail.com>
2024-11-15 12:36:39 +0100

    Advanced calculator with calculation log

--- src/_karlo_dragan/calculator/ConsoleCalculator.java
+++ src/_karlo_dragan/calculator/ConsoleCalculator.java
@@ -1,5 +1,8 @@
 package cofeemachinesvi.src._karlo_dragan.calculator;
 
+
+import java.util.Scanner;
+
 import java.util.Scanner;
 
 public class ConsoleCalculator {
@@ -7,22 +10,42 @@ public class ConsoleCalculator {
     static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
 
-        Calculator calculator = new Calculator();
+        Calculator calculator = null;
 
-        System.out.println("Operacije osnovnog kalkultora:");
+        System.out.print("1 - Basic\n" +
+                "2 - Advanced\n" +
+                "Enter: ");
 
-        while (true) {
-            String choice;
-            System.out.println("1- Calculate");
-            System.out.println("2 - Exit");
-            System.out.print("Enter:");
-            choice = scanner.next();
+        int ch = scanner.nextInt();
 
-            if (choice.equals("2")) return;
+        if (ch == 1) {
+            calculator = new Calculator();
+            System.out.println("Operacije osnovnog kalkultora:");
+            calculator.printOperations();
+            enterNumbersAndOperation(calculator);
+            System.out.println("Result:" + calculator.calculate());
+
+        } else if (ch == 2) {
+            calculator = new AdvancedCalculator();
+            System.out.println("Operacije naprednog kalkultora:");
+            calculator.printOperations();
+            enterNumbersAndOperation(calculator);
+            System.out.println("Result:" + calculator.calculate());
+
+            AdvancedCalculator ac = (AdvancedCalculator) calculator;
+
+            for (CalculationLog h: ac.getHistoryLogList()) {
+                System.out.println("History:" + h.getRecord());
+            }
         }
     }
-
-    public static void enterNumbersAndOperation (){
-
+    public static void enterNumbersAndOperation (Calculator c){
+        System.out.print("A: ");
+        c.setA(scanner.nextDouble());
+        System.out.print("Op: ");
+        c.setOperation(scanner.next());
+        System.out.print("B: ");
+        c.setB(scanner.nextDouble());
     }
 }
+
