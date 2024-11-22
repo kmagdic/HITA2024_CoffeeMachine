c63efc65cab5e0f403d7560cbcffaa08cc1171f8
drc <dadopavlekovic@gmail.com>
2024-11-22 06:44:09 +0100

    calculator with exceptions

--- src/t1_darko/coffeemachine/CoffeeMachine.java
+++ src/t1_darko/coffeemachine/CoffeeMachine.java
@@ -1,23 +1,23 @@
 package t1_darko.coffeemachine;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Scanner;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CoffeeMachine {
 
-    private int water;
-    private int milk;
-    private int coffeeBeans;
-    private int cups;
-    private float money;
-    private CoffeeType[] coffeeTypes = new CoffeeType[3];
+    protected int water;
+    protected int milk;
+    protected int coffeeBeans;
+    protected int cups;
+    protected float money;
+    private List<CoffeeType> coffeeTypes = new ArrayList<>();
 
-    private String adminUsername = "admin";
-    private String adminPassword = "admin12345";
-    private String statusFileName = "docs/coffee_machine_status.txt";
+    protected String adminUsername = "admin";
+    protected String adminPassword = "admin12345";
+
+    // transaction log
+    private List<Transaction> transactionLog = new ArrayList<>();
 
     public CoffeeMachine(int water, int milk, int coffeeBeans, int cups, float money) {
         this.water = water;
@@ -26,12 +26,13 @@ public class CoffeeMachine {
         this.cups = cups;
         this.money = money;
 
-        coffeeTypes[0] = new CoffeeType("Espresso", 350, 0,16,4);
-        coffeeTypes[1] = new CoffeeType("Latte",350, 75,20,7);
-        coffeeTypes[2] = new CoffeeType("Capuccino",200, 100,12,6);
+        // List.of
+        coffeeTypes.add(new CoffeeType("Espresso", 350, 0,16,4));
+        coffeeTypes.add(new CoffeeType("Latte",350, 75,20,7));
+        coffeeTypes.add(new CoffeeType("Cappuccino",200, 100,12,6));
     }
 
-    public CoffeeType[] getCoffeeTypes() {
+    public List<CoffeeType> getCoffeeTypes() {
         return coffeeTypes;
     }
 
@@ -55,6 +56,10 @@ public class CoffeeMachine {
         return money;
     }
 
+    public List<Transaction> getTransactionLog() {
+        return transactionLog;
+    }
+
     public boolean hasEnoughResources(CoffeeType coffeeType){
         if (water >= coffeeType.getWaterNeeded() &&
                 milk >= coffeeType.getMilkNeeded() &&
@@ -72,10 +77,13 @@ public class CoffeeMachine {
             this.coffeeBeans -= coffeeType.getCoffeeBeansNeeded();
             this.cups -= 1;
             this.money += coffeeType.getPrice();
-
+            Transaction buyTransaction = new Transaction(LocalDateTime.now(), coffeeType.getName(), "Bought");
+            transactionLog.add(buyTransaction);
             return "I have enough resources, making you " + coffeeType.getName() + "\n";
         } else {
             String missing = calculateWhichIngredientIsMissing(coffeeType);
+            Transaction notBuyTransaction = new Transaction(LocalDateTime.now(), coffeeType.getName(), "Not Bought", missing);
+            transactionLog.add(notBuyTransaction);
             return "Sorry, not enough " + missing + "\n";
         }
     }
@@ -117,66 +125,33 @@ public class CoffeeMachine {
             return false;
     }
 
-
-    public boolean loadFromFile(String fileName)  {
-        FileReader reader = null;
-
-        try {
-            reader = new FileReader(fileName);
-        } catch (FileNotFoundException e) {
-            return false;
+    // password validation
+    public boolean validateNewAdminPassword (String password) {
+        if (password.length() > 7) {
+            for(char c : password.toCharArray()) {
+                if (Character.isDigit(c)) {
+                    return true;
+                }
+            }
         }
-
-        Scanner fileScanner = new Scanner(reader);
-
-        // FILE format:
-        // <water_status>; <milk_status>; <coffee_beans_status>; <cups_status>; <money_status>
-        // <admin_username>; <admin_password>
-
-        fileScanner.useDelimiter("; |\n"); // delimiter is "; " or "\n" (for the last value)
-
-        water = fileScanner.nextInt();
-        milk = fileScanner.nextInt();
-        coffeeBeans = fileScanner.nextInt();
-        cups = fileScanner.nextInt();
-        money = Float.parseFloat(fileScanner.next());
-
-        adminUsername = fileScanner.next();
-        adminPassword = (fileScanner.next()).trim();
-
-        return true;
-
-
+        return false;
     }
 
-    public void saveToFile(String fileName){
-        try {
-            FileWriter writer = new FileWriter(fileName);
-
-            writer.write(water + "; " +  milk + "; " + coffeeBeans + "; " + cups + "; " + money);
-            writer.write("\n");
-            writer.write(adminUsername + "; " + adminPassword);
-            writer.write("\n");
-
-            writer.close();
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    public String changePassword(String password) {
+        this.adminPassword = password;
+        return "Password is changed";
     }
-
 
     public boolean start() {
-        return loadFromFile(statusFileName);
+        return false;
     }
 
     public void stop() {
-        saveToFile(statusFileName);
     }
 
     @Override
     public String toString() {
-        return "CoffeeMachine{" +
+        return "stage2.CoffeeMachine{" +
                 "water=" + water +
                 ", milk=" + milk +
                 ", coffeeBeans=" + coffeeBeans +
@@ -184,6 +159,5 @@ public class CoffeeMachine {
                 ", money=" + money +
                 '}';
     }
-
 
 }
