d45fd81ad1e557e5be441a2d090b2b1b3d41c179
Bojan Pađen <page0itv@gmail.com>
2024-11-25 01:10:56 +0100

    DBManager converted to singleton

--- src/t3_bojan/coffeemachine/DBManager.java
+++ src/t3_bojan/coffeemachine/DBManager.java
@@ -8,17 +8,26 @@ import java.util.List;
 public class DBManager {
     public static final String TABLE_NAME = "transaction_log";
     private static final String DB_URL_AND_NAME = "jdbc:h2:./docs/transaction_log_bojan";
+    private static DBManager instance;
+    private Connection connection;
 
-    public DBManager() {
-        createTable();
-    }
-
-    public Connection getConnection() {
+    private DBManager() {
         try {
-            return DriverManager.getConnection(DB_URL_AND_NAME);
+            connection = DriverManager.getConnection(DB_URL_AND_NAME);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static DBManager getInstance() {
+        if (instance == null) {
+            instance = new DBManager();
+        }
+        return instance;
+    }
+
+    public static DBManager newInstance() {
+        return new DBManager();
     }
 
     public void createTable() {
@@ -30,7 +39,7 @@ public class DBManager {
                 "missing_ingredient VARCHAR(255)\n" +
                 ");";
 
-        try (Connection connection = getConnection(); Statement stmt = connection.createStatement()) {
+        try (Statement stmt = connection.createStatement()) {
             stmt.execute(sqlCreateTable);
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -41,9 +50,7 @@ public class DBManager {
         String sqlSelect = "SELECT * FROM " + TABLE_NAME;
 
         List<TransactionLog> transactionLogs = new ArrayList<>();
-        Connection connection = null;
         try {
-            connection = getConnection();
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(sqlSelect);
 
@@ -70,5 +77,23 @@ public class DBManager {
         }
 
         return transactionLogs;
+    }
+
+    public void insertTransactionLogToDB(LocalDateTime dateTime, String coffeeTypeName, String transactionAction, String missingIngredient) {
+
+        String insertSqlWithoutMissingIngredient = "INSERT INTO " + DBManager.TABLE_NAME + " (date_time, coffee_name, transaction_action) VALUES (?, ?, ?)";
+        String insertSqlWithMissingIngredient = "INSERT INTO " + DBManager.TABLE_NAME + " (date_time, coffee_name, transaction_action, missing_ingredient) VALUES (?, ?, ?, ?)";
+
+        try (PreparedStatement stmt = connection.prepareStatement(missingIngredient == null ? insertSqlWithoutMissingIngredient : insertSqlWithMissingIngredient)) {
+            stmt.setTimestamp(1, java.sql.Timestamp.valueOf(dateTime));
+            stmt.setString(2, coffeeTypeName);
+            stmt.setString(3, transactionAction);
+            if (missingIngredient != null) {
+                stmt.setString(4, missingIngredient);
+            }
+            stmt.executeUpdate();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
