6b17945fae4e0ce222c6fc9acd9de19e47f548ad
Bojan Pađen <page0itv@gmail.com>
2024-11-28 12:38:06 +0100

    update CoffeeMachine stage 4

--- src/t3_bojan/coffeemachine/CoffeeMachine.java
+++ src/t3_bojan/coffeemachine/CoffeeMachine.java
@@ -1,5 +1,6 @@
 package t3_bojan.coffeemachine;
 
+import java.sql.Connection;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +16,8 @@ public class CoffeeMachine {
     protected String adminPassword = "admin12345";
     protected String statusFileName = "src/t3_bojan/coffeemachine/coffee_machine_status.txt";
     private List<CoffeeType> coffeeTypes = new ArrayList<>();
+    private TransactionLog transactionLog;
+    private CoffeeTypeRepository coffeeTypeRepository;
 
     private final String TRANSACTION_SUCCESS_ACTION = "Bought";
     private final String TRANSACTION_FAIL_ACTION = "Not Bought";
@@ -28,9 +31,9 @@ public class CoffeeMachine {
         this.cups = cups;
         this.money = money;
 
-        coffeeTypes.add(new CoffeeType("Espresso", 350, 0,16,4));
-        coffeeTypes.add(new CoffeeType("Latte",350, 75,20,7));
-        coffeeTypes.add(new CoffeeType("Cappuccino",200, 100,12,6));
+        coffeeTypes.add(new CoffeeType("Espresso", 350, 0, 16, 4));
+        coffeeTypes.add(new CoffeeType("Latte", 350, 75, 20, 7));
+        coffeeTypes.add(new CoffeeType("Cappuccino", 200, 100, 12, 6));
     }
 
     public List<CoffeeType> getCoffeeTypes() {
@@ -57,7 +60,7 @@ public class CoffeeMachine {
         return money;
     }
 
-    public boolean hasEnoughResources(CoffeeType coffeeType){
+    public boolean hasEnoughResources(CoffeeType coffeeType) {
         if (water >= coffeeType.getWaterNeeded() &&
                 milk >= coffeeType.getMilkNeeded() &&
                 coffeeBeans >= coffeeType.getCoffeeBeansNeeded() &&
@@ -67,7 +70,7 @@ public class CoffeeMachine {
             return false;
     }
 
-    public String buyCoffee(CoffeeType coffeeType){
+    public String buyCoffee(CoffeeType coffeeType) {
 
         if (hasEnoughResources(coffeeType)) {
             this.water -= coffeeType.getWaterNeeded();
@@ -75,44 +78,38 @@ public class CoffeeMachine {
             this.coffeeBeans -= coffeeType.getCoffeeBeansNeeded();
             this.cups -= 1;
             this.money += coffeeType.getPrice();
-
-            DBManager.getInstance().insertTransactionLogToDB(LocalDateTime.now(), coffeeType.getName(), TRANSACTION_SUCCESS_ACTION, null);
+            transactionLog = new TransactionLog(LocalDateTime.now(), coffeeType, TRANSACTION_SUCCESS_ACTION);
 
             return I_HAVE_ENOUGH_RESOURCES + coffeeType.getName() + "\n";
         } else {
             String missing = calculateWhichIngredientIsMissing(coffeeType);
-
-            DBManager.getInstance().insertTransactionLogToDB(LocalDateTime.now(), coffeeType.getName(), TRANSACTION_FAIL_ACTION, missing);
+            transactionLog = new TransactionLog(LocalDateTime.now(), coffeeType, TRANSACTION_FAIL_ACTION, missing);
 
             return I_DONT_HAVE_ENOUGH_RESOURCES + missing + "\n";
         }
     }
 
-
-    public float takeMoney(){
+    public float takeMoney() {
         float moneyReturn = money;
         money = 0;
         return moneyReturn;
     }
 
-    public String calculateWhichIngredientIsMissing(CoffeeType coffeeType){
+    public String calculateWhichIngredientIsMissing(CoffeeType coffeeType) {
         String ingredientMissing = null;
         if (water < coffeeType.getWaterNeeded()) {
             ingredientMissing = "water";
-        }
-        else if (milk < coffeeType.getMilkNeeded()) {
-            ingredientMissing = "milk" ;
-        }
-        else if (coffeeBeans < coffeeType.getCoffeeBeansNeeded()) {
-            ingredientMissing = "coffee beans" ;
-        }
-        else if (cups < 1) {
-            ingredientMissing = "cups" ;
+        } else if (milk < coffeeType.getMilkNeeded()) {
+            ingredientMissing = "milk";
+        } else if (coffeeBeans < coffeeType.getCoffeeBeansNeeded()) {
+            ingredientMissing = "coffee beans";
+        } else if (cups < 1) {
+            ingredientMissing = "cups";
         }
         return ingredientMissing;
     }
 
-    public void fill(int water, int milk, int coffeeBeans, int cups){
+    public void fill(int water, int milk, int coffeeBeans, int cups) {
         this.water += water;
         this.milk += milk;
         this.coffeeBeans += coffeeBeans;
@@ -131,8 +128,20 @@ public class CoffeeMachine {
         adminPassword = newPassword;
     }
 
+    public TransactionLog getTransactionLog() {
+        return transactionLog;
+    }
 
-    public boolean start() {
+    public boolean start(Connection connection) {
+        coffeeTypeRepository = new CoffeeTypeRepository(connection);
+        coffeeTypeRepository.createTable();
+        int rows = coffeeTypeRepository.getCoffeeTypeRowCount();
+
+        if (rows < 3) {
+            for (CoffeeType coffeeType : coffeeTypes) {
+                coffeeTypeRepository.insert(coffeeType);
+            }
+        }
         return true;
     }
 
